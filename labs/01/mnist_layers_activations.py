21f610bbaf0947d38a63fd58d04a664639f81bf4
tomiinek <tom@neqindi.cz>
2019-03-13 12:54:37 +0100

    First labs

--- labs/01/mnist_layers_activations.py
+++ labs/01/mnist_layers_activations.py
@@ -9,9 +9,16 @@ import tensorflow as tf
 
 from mnist import MNIST
 
+def get_activation(x):
+    return {
+        'relu':     tf.nn.relu,
+        'tanh':     tf.nn.tanh,
+        'sigmoid':  tf.nn.sigmoid
+    }.get(x, None)
+
 # Parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--activation", default="none", type=str, help="Activation function.")
+parser.add_argument("--activation", default="relu", type=str, help="Activation function.")
 parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
 parser.add_argument("--epochs", default=10, type=int, help="Number of epochs.")
 parser.add_argument("--hidden_layer", default=100, type=int, help="Size of the hidden layer.")
@@ -29,7 +36,7 @@ tf.config.threading.set_inter_op_parallelism_threads(args.threads)
 tf.config.threading.set_intra_op_parallelism_threads(args.threads)
 
 # Create logdir name
-args.logdir = "logs/{}-{}-{}".format(
+args.logdir = "logs\\{}-{}-{}".format(
     os.path.basename(__file__),
     datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
     ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
@@ -39,13 +46,12 @@ args.logdir = "logs/{}-{}-{}".format(
 mnist = MNIST()
 
 # Create the model
-model = tf.keras.Sequential([
-    tf.keras.layers.InputLayer((MNIST.H, MNIST.W, MNIST.C)),
-    tf.keras.layers.Flatten(),
-    # TODO: Add `args.layers` number of hidden layers with size `args.hidden_layer`,
-    # using activation from `args.activation`, allowing "none", "relu", "tanh", "sigmoid".
-    tf.keras.layers.Dense(MNIST.LABELS, activation=tf.nn.softmax),
-])
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.InputLayer((MNIST.H, MNIST.W, MNIST.C)))
+model.add(tf.keras.layers.Flatten())
+for _ in range(args.layers):
+    model.add(tf.keras.layers.Dense(args.hidden_layer, activation=get_activation(args.activation)))
+model.add(tf.keras.layers.Dense(MNIST.LABELS, activation=tf.nn.softmax))
 
 model.compile(
     optimizer=tf.keras.optimizers.Adam(),
@@ -67,6 +73,6 @@ test_logs = model.evaluate(
 )
 tb_callback.on_epoch_end(1, dict(("val_test_" + metric, value) for metric, value in zip(model.metrics_names, test_logs)))
 
-# TODO: Write test accuracy as percentages rounded to two decimal places.
+accuracy = test_logs[1]
 with open("mnist_layers_activations.out", "w") as out_file:
     print("{:.2f}".format(100 * accuracy), file=out_file)
--- labs/01/numpy_entropy.py
+++ labs/01/numpy_entropy.py
@@ -3,27 +3,35 @@ import numpy as np
 
 if __name__ == "__main__":
     # Load data distribution, each data point on a line
+    distribution = {}
     with open("numpy_entropy_data.txt", "r") as data:
         for line in data:
             line = line.rstrip("\n")
-            # TODO: process the line, aggregating using Python data structures
+            if line in distribution:
+                distribution[line] += 1
+            else:
+                distribution[line] = 1
 
-    # TODO: Create a NumPy array containing the data distribution. The
-    # NumPy array should contain only data, not any mapping. If required,
-    # the NumPy array might be created after loading the model distribution.
+        d = np.array([value for (key, value) in sorted(distribution.items())])
+        d = d / d.sum(axis=0)
 
-    # Load model distribution, each line `word \t probability`.
-    with open("numpy_entropy_model.txt", "r") as model:
-        for line in model:
-            line = line.rstrip("\n")
-            # TODO: process the line, aggregating using Python data structures
+        # Load model distribution, each line `word \t probability`, creating
+        # a NumPy array containing the model distribution
+        model_distribution = {key: 0 for key in distribution.keys()}
+        with open("numpy_entropy_model.txt", "r") as model:
+            for line in model:
+                line = line.rstrip("\n")
+                key, value = line.split("\t")
+                if key in model_distribution:
+                    model_distribution[key] = float(value)
 
-    # TODO: Create a NumPy array containing the model distribution.
+        m = np.array([value for (key, value) in sorted(model_distribution.items())])
 
-    # TODO: Compute and print the entropy H(data distribution). You should not use
-    # manual for/while cycles, but instead use the fact that most NumPy methods
-    # operate on all elements (for example `*` is vector element-wise multiplication).
-    print("{:.2f}".format(entropy))
+        entropy = (-d * np.log(d)).sum(axis=0)
+        print("{:.2f}".format(entropy))
 
-    # TODO: Compute and print cross-entropy H(data distribution, model distribution)
-    # and KL-divergence D_KL(data distribution, model_distribution)
+        cross_entropy = (-d * np.log(m)).sum(axis=0)
+        print("{:.2f}".format(cross_entropy))
+
+        kl_div = (d * (np.log(d) - np.log(m))).sum(axis=0)
+        print("{:.2f}".format(kl_div))
