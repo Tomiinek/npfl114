fe96ca6a6b1abc6fcf75905518f50931e8fbaf81
tomiinek <tom@neqindi.cz>
2019-03-16 18:30:49 +0100

    Second task.

--- labs/02/gym_cartpole.py
+++ labs/02/gym_cartpole.py
@@ -7,11 +7,16 @@ import re
 import numpy as np
 import tensorflow as tf
 
+def get_activation(x):
+    return {'relu': tf.nn.relu, 'tanh': tf.nn.tanh, 'sigmoid':  tf.nn.sigmoid}.get(x, None)
+
 # Parse arguments
-# TODO: Set reasonable defaults and possibly add more arguments.
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size", default=None, type=int, help="Batch size.")
-parser.add_argument("--epochs", default=None, type=int, help="Number of epochs.")
+parser.add_argument("--batch_size", default=1, type=int, help="Batch size.")
+parser.add_argument("--epochs", default=1000, type=int, help="Number of epochs.")
+parser.add_argument("--activation", default="tanh", type=str, help="Hidden layer activation.")
+parser.add_argument("--hidden_layer", default=5, type=int, help="Size of the hidden layer.")
+parser.add_argument("--learning_rate", default=0.0005, type=float, help="Learning rate.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
 args = parser.parse_args()
 
@@ -22,13 +27,15 @@ tf.config.threading.set_inter_op_parallelism_threads(args.threads)
 tf.config.threading.set_intra_op_parallelism_threads(args.threads)
 
 # Create logdir name
-args.logdir = "logs/{}-{}-{}".format(
+args.logdir = "logs\\{}-{}-{}".format(
     os.path.basename(__file__),
     datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
     ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
 )
 
 # Load the data
+OBSERVATIONS = 4
+ACTIONS = 2
 observations, labels = [], []
 with open("gym_cartpole-data.txt", "r") as data:
     for line in data:
@@ -37,14 +44,14 @@ with open("gym_cartpole-data.txt", "r") as data:
         labels.append(int(columns[-1]))
 observations, labels = np.array(observations), np.array(labels)
 
-# TODO: Create the model in the `model` variable.
-# However, beware that there is currently a bug in Keras which does
-# not correctly serialize InputLayer. Instead of using an InputLayer,
-# pass explicitly `input_shape` to the first real model layer.
-model = None
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=[OBSERVATIONS]),
+    tf.keras.layers.Dense(args.hidden_layer, activation=get_activation(args.activation)),
+    tf.keras.layers.Dense(ACTIONS, activation=tf.nn.softmax),
+])
 
 model.compile(
-    optimizer=tf.keras.optimizers.Adam(),
+    optimizer=tf.keras.optimizers.Adam(lr=args.learning_rate),
     loss=tf.keras.losses.SparseCategoricalCrossentropy(),
     metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
 )
--- labs/02/mnist_training.py
+++ labs/02/mnist_training.py
@@ -9,6 +9,24 @@ import tensorflow as tf
 
 from mnist import MNIST
 
+
+class LearningRateHistory(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, batch, logs={}):
+        print(self.model.optimizer.learning_rate(self.model.optimizer.iterations))
+
+
+def get_optimizer(optimizer, decay, training_examples):
+    decay_steps = args.epochs * training_examples // args.batch_size
+    learning_rate = args.learning_rate
+    if decay == 'polynomial':
+        learning_rate = tf.keras.optimizers.schedules.PolynomialDecay(args.learning_rate, decay_steps, args.learning_rate_final)
+    if decay == 'exponential':
+        learning_rate = tf.keras.optimizers.schedules.ExponentialDecay(args.learning_rate, decay_steps, args.learning_rate_final / args.learning_rate)
+    m = args.momentum if args.momentum else 0.0
+    return {'SGD':  tf.keras.optimizers.SGD(learning_rate=learning_rate, momentum=m),
+            'Adam': tf.keras.optimizers.Adam(learning_rate=learning_rate)}.get(optimizer)
+
+
 # Parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
@@ -16,11 +34,11 @@ parser.add_argument("--decay", default=None, type=str, help="Learning decay rate
 parser.add_argument("--epochs", default=10, type=int, help="Number of epochs.")
 parser.add_argument("--hidden_layer", default=200, type=int, help="Size of the hidden layer.")
 parser.add_argument("--learning_rate", default=0.01, type=float, help="Initial learning rate.")
-parser.add_argument("--learning_rate_final", default=None, type=float, help="Final learning rate.")
+parser.add_argument("--learning_rate_final", default=0.001, type=float, help="Final learning rate.")
 parser.add_argument("--momentum", default=None, type=float, help="Momentum.")
 parser.add_argument("--optimizer", default="SGD", type=str, help="Optimizer to use.")
 parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
-parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+parser.add_argument("--threads", default=4, type=int, help="Maximum number of threads to use.")
 args = parser.parse_args()
 
 # Fix random seeds
@@ -48,33 +66,20 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(MNIST.LABELS, activation=tf.nn.softmax),
 ])
 
-# TODO: Use the required `args.optimizer` (either `SGD` or `Adam`).
-# For `SGD`, `args.momentum` can be specified. If `args.decay` is
-# not specified, pass the given `args.learning_rate` directly to the
-# optimizer. If `args.decay` is set, then
-# - for `polynomial`, use `tf.keras.optimizers.schedules.PolynomialDecay`
-#   using the given `args.learning_rate_final`;
-# - for `exponential`, use `tf.keras.optimizers.schedules.ExponentialDecay`
-#   and set `decay_rate` appropriately to reach `args.learning_rate_final`
-#   just after the training (and keep the default `staircase=False`).
-# In both cases, `decay_steps` should be total number of training batches.
-# If a learning rate schedule is used, you can find out current learning rate
-# by using `model.optimizer.learning_rate(model.optimizer.iterations)`,
-# so after training this value should be `args.learning_rate_final`.
-
 model.compile(
-    optimizer=None,
+    optimizer=get_optimizer(args.optimizer, args.decay, mnist.train.size),
     loss=tf.keras.losses.SparseCategoricalCrossentropy(),
     metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
 )
 
+lr_callback=LearningRateHistory()
 tb_callback=tf.keras.callbacks.TensorBoard(args.logdir, histogram_freq=1, update_freq=1000, profile_batch=1)
 tb_callback.on_train_end = lambda *_: None
 model.fit(
     mnist.train.data["images"], mnist.train.data["labels"],
     batch_size=args.batch_size, epochs=args.epochs,
     validation_data=(mnist.dev.data["images"], mnist.dev.data["labels"]),
-    callbacks=[tb_callback],
+    callbacks=[tb_callback],  # lr_callback],
 )
 
 test_logs = model.evaluate(
@@ -82,6 +87,6 @@ test_logs = model.evaluate(
 )
 tb_callback.on_epoch_end(1, dict(("val_test_" + metric, value) for metric, value in zip(model.metrics_names, test_logs)))
 
-# TODO: Write test accuracy as percentages rounded to two decimal places.
+accuracy = test_logs[1]
 with open("mnist_training.out", "w") as out_file:
     print("{:.2f}".format(100 * accuracy), file=out_file)
